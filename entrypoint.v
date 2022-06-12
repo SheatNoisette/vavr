650ee1fe09ffe3ad41abd63542ab9b9878508370
SheatNoisette <sheatnoisette@protonmail.com>
2022-06-12 18:03:45 +0200

    vavr: Simplify code

--- entrypoint.v
+++ entrypoint.v
@@ -2,30 +2,20 @@ module main
 
 #define EMPTY_STRUCT_DECLARATION
 #define VV_LOCAL_SYMBOL
-#define _vcleanup  main__v_cleanup
-#define _vinit     main__v_init
-
-#define GC_INIT() {}
-#define g_main_argc int fargc
-#define g_main_argv char** fargv
-#define GC_set_pages_executable(x) {}
+#define VV_EXPORTED_SYMBOL
 
 #define voidptr void*
-#define array voidptr
 #define u32 int
-
 
 #include <avr/io.h>
 #include <util/delay.h>
 
-[markused] fn v_init(argc int, argv &&char) {
+[export: 'main']
+[markused]
+fn vmain(argc int, argv &&char) int {
 	setup()
-}
-
-[markused] fn v_cleanup() {}
-
-fn main() {
 	for {
 		loop()
 	}
+	return 0
 }
